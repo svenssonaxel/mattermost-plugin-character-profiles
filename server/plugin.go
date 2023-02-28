ddeb8b43be6e5aa3c2457999ff1fb3a01d233bef
Axel Svensson <mail@axelsvensson.com>
2023-02-28 07:41:09 +0100

    Minimal NPC functionality

--- server/plugin.go
+++ server/plugin.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"regexp"
 	"sync"
 
+	"github.com/mattermost/mattermost-server/v6/model"
 	"github.com/mattermost/mattermost-server/v6/plugin"
 )
 
@@ -16,4 +18,37 @@ type Plugin struct {
 	// configuration is the active plugin configuration. Consult getConfiguration and
 	// setConfiguration for usage.
 	configuration *configuration
+}
+
+func (p *Plugin) MessageWillBePosted(_ *plugin.Context, post *model.Post) (*model.Post, string) {
+	return p.NpcPost(post)
+}
+
+func (p *Plugin) MessageWillBeUpdated(_ *plugin.Context, newPost *model.Post, _ *model.Post) (*model.Post, string) {
+	return p.NpcPost(newPost)
+}
+
+func (p *Plugin) NpcPost(post *model.Post) (*model.Post, string) {
+	// Only touch posts created by users
+	if post.IsSystemMessage() || post.UserId == "" {
+		return nil, ""
+	}
+	// Check if the post message matches the regex pattern
+	pattern := regexp.MustCompile(`(?s)^npc ([a-z]+): ?(.*)$`)
+	matches := pattern.FindStringSubmatch(post.Message)
+	ret := post.Clone()
+	if len(matches) == 3 {
+		// This is an NPC post, so override the displayed user name and message
+		ret.AddProp("override_username", matches[1])
+		ret.AddProp("from_webhook", "true") // Unfortunately we need to pretend this is from a bot, or the username won't get overridden.
+		//ret.Props["override_icon_url"] = "https://example.com/my-custom-bot-icon.png"
+		ret.MessageSource = ret.Message
+		ret.Message = matches[2]
+		return ret, ""
+	} else {
+		// This is a user post, so make sure to use the defaults.
+		ret.DelProp("override_username")
+		ret.AddProp("from_webhook", "false")
+		return ret, ""
+	}
 }
